0587fd15326c7528ea3b3a5e1862163764947d2e
Anthony Shellman <atshellman@gmail.com>
2025-05-09 17:15:03 -0600

    import sys, remove hardcoded book path, add usage message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_sorted_list_of_dict
+import sys
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -6,7 +7,11 @@ def get_book_text(filepath):
   return get_num_words(file_contents)
 
 def main():
-  path_to_file = "books/frankenstein.txt"
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+  path_to_file = sys.argv[1]
   word_count_message, ocurrences = get_book_text(path_to_file)
 
   reformatted_ocurrences = get_sorted_list_of_dict(ocurrences)
